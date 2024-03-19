82f8f2fb6302e3a10fb1aa31507a1c794681a8b6
Jefferson Schmitt <jeffersonschmitt@live.com>
2024-03-19 11:19:14 -0300

    Update Api

--- src/data/services/api.js
+++ src/data/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const searchImages = async (term) => {
   const response = await axios.get('https://api.unsplash.com/search/photos', {
     headers: {
-      Authorization: 'Client-ID FkPgsNchDpcOMVmJ-NYdD9z0AtDEenqXCtcSI4DpHrA'
+      Authorization: 'Client-ID yourkeyunsplash'
     },
     params: {
       query: term,
